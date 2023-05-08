a0650a1ef4c8899399abc583f2eae64c11608d3a
Shalom Taiwo <shalomtaiwo@my.uopeople.edu>
2023-05-08 07:46:54 +0200

    log in and logout button

--- src/Components/Header/Navbar/UserAccount.js
+++ src/Components/Header/Navbar/UserAccount.js
@@ -2,14 +2,18 @@ import { Menu, ActionIcon } from "@mantine/core";
 import {
 	IconSettings,
 	IconUserCircle,
-    IconDashboard,
-    IconCategory,
-    IconHomeEdit,
-    IconDoorExit,
+	IconDashboard,
+	IconCategory,
+	IconHomeEdit,
+	IconDoorExit,
 } from "@tabler/icons-react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { NavLink } from "react-router-dom";
+import { auth } from "../../../Firebase-config";
 
 export default function UserAccount() {
+	const [user] = useAuthState(auth);
+
 	return (
 		<Menu
 			shadow="md"
@@ -23,26 +27,42 @@ export default function UserAccount() {
 
 			<Menu.Dropdown>
 				<Menu.Label>Account</Menu.Label>
-				<NavLink to={"/account"} className={'navlink'}>
+				<NavLink
+					to={"/account"}
+					className={"navlink"}
+				>
 					<Menu.Item icon={<IconDashboard size={14} />}>Dashboard</Menu.Item>
 				</NavLink>
-                <NavLink to={"/account/orders"} className={'navlink'}>
+				<NavLink
+					to={"/account/orders"}
+					className={"navlink"}
+				>
 					<Menu.Item icon={<IconCategory size={14} />}>Orders</Menu.Item>
 				</NavLink>
-                <NavLink to={"/account/settings"} className={'navlink'}>
+				<NavLink
+					to={"/account/settings"}
+					className={"navlink"}
+				>
 					<Menu.Item icon={<IconSettings size={14} />}>Settings</Menu.Item>
 				</NavLink>
-                <NavLink to={"/account/addresses"} className={'navlink'}>
+				<NavLink
+					to={"/account/addresses"}
+					className={"navlink"}
+				>
 					<Menu.Item icon={<IconHomeEdit size={14} />}>Addresses</Menu.Item>
 				</NavLink>
 
-				<Menu.Divider />
-				<Menu.Item
-					color="red"
-					icon={<IconDoorExit size={14} />}
-				>
-					Logout
-				</Menu.Item>
+				{user && (
+					<>
+						<Menu.Divider />
+						<Menu.Item
+							color="red"
+							icon={<IconDoorExit size={14} />}
+						>
+							Logout
+						</Menu.Item>
+					</>
+				)}
 			</Menu.Dropdown>
 		</Menu>
 	);
